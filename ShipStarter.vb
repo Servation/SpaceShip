9e6248d8edfcc659c1e5b3b5b1488bbc366cb131
Jeffrey Saelee <gwfds123@gmail.com>
2021-03-11 19:15:26 -0800

    change max speed to 5

--- ShipStarter.vb
+++ ShipStarter.vb
@@ -3,7 +3,7 @@
     Public y As Decimal
     Public speedX As Decimal
     Public speedY As Decimal
-    Public maxSpeed = 15
+    Public maxSpeed = 5
     Private MainRect As Rectangle
     Public visible = True
 
