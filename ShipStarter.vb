d998e213b83846bcfec1f2b439fc2b061cea3b46
Jeffrey Saelee <gwfds123@gmail.com>
2021-03-11 19:02:45 -0800

    test

--- ShipStarter.vb
+++ ShipStarter.vb
@@ -3,7 +3,7 @@
     Public y As Decimal
     Public speedX As Decimal
     Public speedY As Decimal
-    Public maxSpeed = 5
+    Public maxSpeed = 10
     Private MainRect As Rectangle
     Public visible = True
 
